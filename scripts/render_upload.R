311312ada9938d8a9fa86150e16a9783e8d1616c
François <francois.delavy@gmail.com>
2021-06-10 13:21:23 -0400

    first version sent to Daniel

--- scripts/render_upload.R
+++ scripts/render_upload.R
@@ -2,8 +2,11 @@ rmarkdown::render(here::here("rmd", "final.Rmd"))
 
 # name study like FTP_CTR_STUDY in environ-file
 usethis::edit_r_environ()
+# add this line there: FTP_CTR_STUDY="ftp://finantj:F6Y85df4xAxD0v8I@www457.your-server.de//public_html/frontpagedata/html_files/ctr-study.html"
+# save the .Renviron
+
 
 # add study name under Sys.getenv 
-RCurl::ftpUpload(here::here("rmd", "2_final.html"), Sys.getenv("FTP_CTR_STUDY"))
+RCurl::ftpUpload(here::here("rmd", "03_final.html"), Sys.getenv("FTP_CTR_STUDY"))
 
 
