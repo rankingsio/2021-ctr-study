9847dcfaa29a1fd119137eabaf605b59364e75af
danielkupka <d.kupka@hotmail.com>
2021-07-27 11:14:59 +0200

    plot graph title change

--- scripts/render_upload.R
+++ scripts/render_upload.R
@@ -2,7 +2,7 @@ rmarkdown::render(here::here("rmd", "final.Rmd"))
 
 # name study like FTP_CTR_STUDY in environ-file
 usethis::edit_r_environ()
-# add this line there: FTP_CTR_STUDY="ftp://finantj:F6Y85df4xAxD0v8I@www457.your-server.de//public_html/frontpagedata/html_files/ctr-study.html"
+# add this line there: FTP_CTR_STUDY="ftp://finantj:THEPWISENTYOU@www457.your-server.de//public_html/frontpagedata/html_files/ctr-study.html"
 # save the .Renviron. Restart RStudio
 
 # then, finally, run this:  
